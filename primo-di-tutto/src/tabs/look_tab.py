416100103254549903788d342d92c9a833cff0e4
Timo Westphal <actionschnitzel@posteo.de>
2024-10-07 15:59:10 +0200

    Improved:Ubuntu Layout pt2

--- primo-di-tutto/src/tabs/look_tab.py
+++ primo-di-tutto/src/tabs/look_tab.py
@@ -85,11 +85,46 @@ class LookTab(ttk.Frame):
                 'gsettings', 'set', 'org.cinnamon', 'panels-enabled', "['1:0:top', '2:0:left']"
             ])
 
+            subprocess.run([
+                'gsettings', 'set', 'org.cinnamon', 'panels-height', "['1:32', '2:50']"
+            ])
+
+            subprocess.run([
+                'gsettings', 'set', 'org.cinnamon', 'enabled-extensions', "['transparent-panels@germanfr']"
+            ])
+
+            subprocess.run([
+                'gsettings', 'set', 'org.cinnamon', 'panel-zone-icon-sizes', """[{"panelId":1,"left":0,"center":0,"right":24},{"left":32,"center":0,"right":32,"panelId":2}]"""
+            ])
+
+
             # Zweiter Befehl: Setze die aktivierten Applets
             subprocess.run([
                 'gsettings', 'set', 'org.cinnamon', 'enabled-applets',
-                "['panel2:right:0:menu@cinnamon.org:0', 'panel1:left:1:separator@cinnamon.org:1', 'panel2:left:0:grouped-window-list@cinnamon.org:2', 'panel1:right:0:systray@cinnamon.org:3', 'panel1:right:1:xapp-status@cinnamon.org:4', 'panel1:right:2:notifications@cinnamon.org:5', 'panel1:right:3:printers@cinnamon.org:6', 'panel1:right:4:removable-drives@cinnamon.org:7', 'panel1:right:5:keyboard@cinnamon.org:8', 'panel1:right:6:favorites@cinnamon.org:9', 'panel1:right:7:network@cinnamon.org:10', 'panel1:right:8:sound@cinnamon.org:11', 'panel1:right:9:power@cinnamon.org:12', 'panel1:right:10:calendar@cinnamon.org:13', 'panel1:right:11:cornerbar@cinnamon.org:14']"
+                "['panel2:right:0:menu@cinnamon.org:0', 'panel1:left:1:separator@cinnamon.org:1', 'panel2:left:0:grouped-window-list@cinnamon.org:2', 'panel1:right:0:systray@cinnamon.org:3', 'panel1:right:1:xapp-status@cinnamon.org:4', 'panel1:right:2:notifications@cinnamon.org:5', 'panel1:right:3:printers@cinnamon.org:6', 'panel1:right:4:removable-drives@cinnamon.org:7', 'panel1:right:5:keyboard@cinnamon.org:8', 'panel1:right:6:favorites@cinnamon.org:9', 'panel1:right:7:network@cinnamon.org:10', 'panel1:right:8:sound@cinnamon.org:11', 'panel1:right:9:power@cinnamon.org:12', 'panel1:center:0:calendar@cinnamon.org:13', 'panel1:right:11:cornerbar@cinnamon.org:14']"
             ])
+
+            config_path = os.path.expanduser("~/.config/cinnamon/spices/transparent-panels@germanfr/transparent-panels@germanfr.json")
+
+            # Konfigurationsdatei lesen
+            with open(config_path, 'r') as file:
+                config = json.load(file)
+
+            # Werte aktualisieren
+            config["transparency-type"]["value"] = "panel-semi-transparent"
+            config["panel-left"]["value"] = True
+
+            # Konfigurationsdatei speichern
+            with open(config_path, 'w') as file:
+                json.dump(config, file, indent=4)
+
+            #subprocess.run([
+            #    'cinnamon', '--replace']
+            #)
+
+
+
+            print("Konfiguration erfolgreich aktualisiert.")
 
         def set_upside_down_panel():
             # Erster Befehl: Setze das Panel oben
