d1fb952f150219fd527ad140ba43758f7eea5e96
Timo Westphal <actionschnitzel@posteo.de>
2024-10-08 02:48:50 +0200

    Improved: Ubuntu Layout

--- primo-di-tutto/src/tabs/look_tab.py
+++ primo-di-tutto/src/tabs/look_tab.py
@@ -177,7 +177,7 @@ class LookTab(ttk.Frame):
             # Zweiter Befehl: Setze die aktivierten Applets
             subprocess.run([
                 'gsettings', 'set', 'org.cinnamon', 'enabled-applets',
-                "['panel2:right:0:menu@cinnamon.org:0', 'panel1:left:1:separator@cinnamon.org:1', 'panel2:left:0:grouped-window-list@cinnamon.org:2', 'panel1:right:0:systray@cinnamon.org:3', 'panel1:right:1:xapp-status@cinnamon.org:4', 'panel1:right:2:notifications@cinnamon.org:5', 'panel1:right:3:printers@cinnamon.org:6', 'panel1:right:4:removable-drives@cinnamon.org:7', 'panel1:right:5:keyboard@cinnamon.org:8', 'panel1:right:6:favorites@cinnamon.org:9', 'panel1:right:7:network@cinnamon.org:10', 'panel1:right:8:sound@cinnamon.org:11', 'panel1:right:9:power@cinnamon.org:12', 'panel1:center:0:calendar@cinnamon.org:13', 'panel1:right:11:cornerbar@cinnamon.org:14']"
+                "['panel2:right:0:menu@cinnamon.org:0', 'panel2:left:0:grouped-window-list@cinnamon.org:2', 'panel1:right:3:systray@cinnamon.org:3', 'panel1:right:4:xapp-status@cinnamon.org:4', 'panel1:right:5:notifications@cinnamon.org:5', 'panel1:right:6:printers@cinnamon.org:6', 'panel1:right:7:removable-drives@cinnamon.org:7', 'panel1:right:8:keyboard@cinnamon.org:8', 'panel1:right:9:favorites@cinnamon.org:9', 'panel1:right:10:network@cinnamon.org:10', 'panel1:right:11:sound@cinnamon.org:11', 'panel1:right:12:power@cinnamon.org:12', 'panel1:center:0:calendar@cinnamon.org:13', 'panel1:right:14:cornerbar@cinnamon.org:14', 'panel1:left:1:expo@cinnamon.org:17']"
             ])
 
             
