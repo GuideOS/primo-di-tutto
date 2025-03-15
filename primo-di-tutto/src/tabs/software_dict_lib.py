a64d497a7c4c90e8653623524db941b966ae9ab8
actionschnitzel <actionschnitzel@guideos.de>
2025-03-15 21:19:49 +0100

    Fixed: Steam not installing

--- primo-di-tutto/src/tabs/software_dict_lib.py
+++ primo-di-tutto/src/tabs/software_dict_lib.py
@@ -128,7 +128,7 @@ class SoftwareGamingTools:
             "Description": "Steam ist eine Plattform zum Herunterladen, Kaufen und Spielen von Spielen.\n\nINFO: Beim Erststart von Steam öffnet sich ein Terminal. Lese aufmerksam und bestätige immer mit 'Y' bzw. 'J' oder 'Enter'. Nach der Installation muss in den Einstellungen SteamPlay aktivert werden.",
             "Icon": f"{application_path}/images/apps/com.valvesoftware.Steam-icon.png",
             "Thumbnail": f"{application_path}/images/apps/com.valvesoftware.Steam-thumb.png",
-            "Install": f"{application_path}/scripts/install_steam",
+            "Install": f"pkexec {application_path}/scripts/install_steam",
             "Uninstall": "pkexec apt remove steam-launcher -y",
             "Path": "steam-launcher",
             "AppStream": "com.valvesoftware.Steam",
