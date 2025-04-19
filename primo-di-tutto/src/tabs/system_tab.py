014056c0d9de88a9ae44a0da074dc5734fd1fdbd
actionschnitzel <actionschnitzel@guideos.de>
2025-04-19 17:38:48 +0200

    Added: Canvas to Tools

--- primo-di-tutto/src/tabs/system_tab.py
+++ primo-di-tutto/src/tabs/system_tab.py
@@ -20,15 +20,39 @@ class SystemTab(ttk.Frame):
         self.grid(row=0, column=0, sticky="nsew")
         self.update_interval = 1000
 
-        sys_btn_frame = ttk.LabelFrame(self, text="Werkzeuge", padding=20)
-        sys_btn_frame.pack(pady=20, padx=20, fill="both", expand=tk.TRUE)
+        
+        # Frame für Canvas und Scrollbar
+        canvas_frame = ttk.Frame(self)
+        canvas_frame.pack(fill=tk.BOTH, expand=True,padx=10, pady=20)
+
+        # Canvas-Container für sys_btn_frame
+        canvas = tk.Canvas(canvas_frame,borderwidth=0, highlightthickness=0)
+        canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+        # Scrollbar hinzufügen
+        scrollbar = ttk.Scrollbar(canvas_frame, orient=tk.VERTICAL, command=canvas.yview)
+        scrollbar.pack(side=tk.RIGHT, fill=tk.Y,padx=5)
+
+        # Frame in den Canvas einfügen
+        scrollable_frame = ttk.Frame(canvas)
+        scrollable_frame.bind(
+            "<Configure>",
+            lambda e: canvas.configure(scrollregion=canvas.bbox("all"))
+        )
+
+        canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
+        canvas.configure(yscrollcommand=scrollbar.set)
+
+        # sys_btn_frame in das scrollbare Frame einfügen
+        sys_btn_frame = ttk.LabelFrame(scrollable_frame, text="Werkzeuge", padding=20)
+        sys_btn_frame.pack(fill="both", expand=tk.TRUE)
 
         sys_btn_frame.grid_columnconfigure(0, weight=2)
         sys_btn_frame.grid_columnconfigure(1, weight=2)
         sys_btn_frame.grid_columnconfigure(2, weight=2)
         sys_btn_frame.grid_columnconfigure(3, weight=1)
         sys_btn_frame.grid_columnconfigure(4, weight=2)
-
+ 
         def sys_btn_action(sys_key):
             # SoftwareSys.sys_dict[sys_key]["Action"]
             command = SoftwareSys.sys_dict[sys_key]["Action"]
@@ -54,13 +78,19 @@ class SystemTab(ttk.Frame):
                 command=lambda key=sys_key: sys_btn_action(key),
                 compound=tk.TOP,
                 style="Custom.TButton",
-                width=20
+                width=19
             )
-            sys_button.grid(row=row, column=column, padx=5, pady=5, sticky="nesw")
+            sys_button.grid(row=row, column=column, padx=3, pady=3, sticky="nesw")
 
             # Hover- und Leave-Ereignisse für diesen Button hinzufügen
             sys_button.bind("<Enter>", lambda event, key=sys_key: self.on_hover(event, key))
             sys_button.bind("<Leave>", self.on_leave)
+
+##############################
+
+
+
+
 
         sys_info_frame = ttk.LabelFrame(self, text="Info", padding=20)
         sys_info_frame.pack(pady=20, padx=20, fill="both")
