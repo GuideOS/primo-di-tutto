077abb3e27a0dc299fbe8bd1b81fd1072e4d09c6
Actionschnitzel <actionschnitzel@guideos.de>
2025-08-28 17:16:39 -0400

    Improofed: Lutris Install

--- primo-di-tutto/src/tabs/welcome_tab.py
+++ primo-di-tutto/src/tabs/welcome_tab.py
@@ -83,7 +83,7 @@ GuideOS richtet sich nicht nur an Anfänger und Umsteiger, sondern lädt alle In
         self.welcome_text_label.pack(pady=10)
 
         # Button to install NVIDIA drivers
-        if has_nvidia_gpu():
+        if not has_nvidia_gpu():
             self.nvidia_button = ttk.Button(
                 self,
                 text=_("NVIDIA-Manager öffnen"),image=self.nvidia_icon, compound="left",
